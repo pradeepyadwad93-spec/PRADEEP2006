b31b483dffccbc1d242bbfd1c67e19ec3b59638d
PRADEEP2006 <pradeepyadwad93@gmail.com>
2025-09-29 19:59:32 +0530

    Add files via upload

---
+++ tutedude 3.py
@@ -0,0 +1,5 @@
+n = int(input("enter the number:"))
+if (n%2==0):
+    print("the enterd number is even")
+else:
+    print("the enterd number is odd")
---
+++ tutedude 4.py
@@ -0,0 +1,5 @@
+a=0
+for i in range(51):
+    a =a+i
+    print(a)
+print("the sum of 1 to 50 is:",a)
