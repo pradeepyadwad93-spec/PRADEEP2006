8f7cf4942a148def0a497b6d65a8cce6e2c1deab
PRADEEP2006 <pradeepyadwad93@gmail.com>
2025-10-02 12:40:58 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+result = factorial(7)
+print(result)
---
+++ task2.py
@@ -0,0 +1,8 @@
+import math
+
+def maths(a):
+    print(a**2)
+    print(math.log(a))
+    print(math.sin(a))
+
+maths(25)
